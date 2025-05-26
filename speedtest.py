41f081182c47baa514544b62187443ef550be37b
Joewolf062 <119711053+Joewolf062@users.noreply.github.com>
2025-05-26 11:58:41 +0200

    Update speedtest.py

--- speedtest.py
+++ speedtest.py
@@ -1417,7 +1417,7 @@ class Speedtest(object):
 
         return self.servers
 
-    def get_closest_servers(self, limit=45):
+    def get_closest_servers(self, limit=105):
         """Limit servers to the closest speedtest.net servers based on
         geographic distance
         """
